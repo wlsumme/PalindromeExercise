2397c3626cd799297ada551fd8a1ea7813302fec
William Summe <wlsumme@gmail.com>
2024-05-31 13:15:53 -0500

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = new string(word.Reverse().ToArray());
+            if(word == reversed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("kakak", true)]
+        [InlineData("racecar", true)]
+
+        public void Test1(string word, bool expected)
         {
+            var pal = new WordSmith();
+
+            var actual = pal.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
